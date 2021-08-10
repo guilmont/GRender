dd1d0d9087f6b007edb1cbac20465e6348363649
GM Oliveira <guigm.oliveira@gmail.com>
2021-08-10 12:59:30 +0200

    Restructuring library

---
+++ GRender/include/application.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "core.h"
+
+// local
+#include "quad.h"
+#include "shader.h"
+#include "framebuffer.h"
+#include "texture.h"
+#include "events.h"
+#include "camera.h"
+#include "fonts.h"
+#include "dialog.h"
+
+
+namespace GRender {
+
+    struct Window
+    {
+        std::string title = "Application";
+        glm::vec2 position = { 0.0f, 0.0f }, size = { 10.0f, 10.0f }; // Just whatever
+        GLFWwindow* ptr = nullptr;
+    };
+
+    class Application
+    {
+    public:
+        Application(void) = default;
+        virtual ~Application(void);
+
+        // Flow control
+        void initialize(const std::string& name, uint32_t width, uint32_t height, const char *layout = NULL);
+        void closeApp(void) { glfwSetWindowShouldClose(window.ptr, 1); }
+        void run(void);
+
+        // To be implemented by user
+        virtual void onUserUpdate(float deltaT) = 0;
+        virtual void ImGuiLayer(void) {}
+        virtual void ImGuiMenuLayer(void) {}
+
+        // Basic utilities
+        Window window;
+        Fonts fonts;
+        Mouse mouse;
+        Camera camera;
+        Keyboard keyboard;
+        Shader shader;
+        Dialog dialog;
+        Texture texture;
+
+    private:
+        float deltaTime = 0.1f;             // This value is going to be uploaded by main loop
+        
+        // Callback functions to control flow
+        friend void winResize_callback(GLFWwindow*, int, int);
+        friend void winPos_callback(GLFWwindow*, int, int);
+        friend void mousePos_callback(GLFWwindow*, double, double);
+        friend void mouseScroll_callback(GLFWwindow*, double, double);
+        friend void mouseButton_callback(GLFWwindow*, int, int, int);
+        friend void keyboard_callback(GLFWwindow*, int, int, int, int);
+        friend void winDrop_callback(GLFWwindow*, int, const char**);
+    };
+
+}
---
+++ GRender/include/shader.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "core.h"
+
+namespace GRender
+{
+
+    class Shader
+    {
+    public:
+        Shader(void) = default;
+        ~Shader(void) = default;
+
+        void deletePrograms(void); // So we can delete all programs before glfw terminates
+
+        bool loadShader(const std::string &label, const fs::path &vtxPath, const fs::path &frgPath);
+
+        void useProgram(const std::string &name);
+
+        void setInteger(const std::string &, int);
+        void setFloat(const std::string &, float);
+        void setVec2f(const std::string &, const float *);
+        void setVec3f(const std::string &, const float *);
+        void setVec4f(const std::string &, const float *);
+        void setMatrix3f(const std::string &, const float *);
+        void setMatrix4f(const std::string &, const float *);
+
+        void setIntArray(const std::string &, const int *, int);
+
+        void setFloatArray(const std::string &, const float *, int);
+        void setVec2fArray(const std::string &, const float *, int);
+        void setVec3fArray(const std::string &, const float *, int);
+        void setMat3Array(const std::string &, const float *, int);
+
+    private:
+        uint32_t program_used = 0;
+        std::unordered_map<std::string, uint32_t> vProgram;
+
+    }; // class-shadear
+
+}
