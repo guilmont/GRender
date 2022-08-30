e7178ff8b006f74a0295c96b67676fad77dd8b57
GM Oliveira <guigm.oliveira@gmail.com>
2022-08-30 20:32:04 +0200

    Refactor viewport -> Now viewport inherits from framebuffer, reducing code duplication

--- GRender/include/GRender/viewport.h
+++ GRender/include/GRender/viewport.h
@@ -5,7 +5,7 @@
 
 namespace GRender {
 
-class Viewport {
+class Viewport : public Framebuffer {
 public:
 	Viewport(void) = default;
 	~Viewport(void) = default;
@@ -22,22 +22,14 @@ public:
 	Viewport(Viewport&& fBuffer) noexcept;
 	Viewport& operator=(Viewport&& fBuffer) noexcept;
 
-	// Utilities to bind and unbing attached framebuffer
-	void bind() const { m_Buffer.bind(); }
-	void unbind() const { m_Buffer.unbind(); }
-
 	// The viewport properties are controlled by the UI
 	bool hovered(void) const { return m_IsHovered; }
-
-	const glm::uvec2& position(void) const { return m_Position; }
-	const glm::uvec2& size(void) const { return m_Buffer.size(); }
+	glm::uvec2 position(void) const { return m_Position; }
 
 	// Creates a ImGui Windows and display attached framebuffer
 	void display(const std::string& windowName = "Viewport", uint32_t attachementID = 0);
 
 private:
-	Framebuffer m_Buffer;
-
 	bool m_IsHovered = false;
 	glm::uvec2 m_Position = { 0,0 };
 };
--- GRender/src/viewport.cpp
+++ GRender/src/viewport.cpp
@@ -3,13 +3,13 @@
 namespace GRender {
 
 Viewport::Viewport(const glm::uvec2& size, const std::vector<texture::Specification>& vSpecs, bool createDepthBuf)
-	: m_Buffer(size, vSpecs, createDepthBuf) {}
+	: Framebuffer(size, vSpecs, createDepthBuf) {}
 
 
 Viewport::Viewport(Viewport&& vp) noexcept {
-	std::swap(m_Buffer, vp.m_Buffer);
 	std::swap(m_IsHovered, vp.m_IsHovered);
 	std::swap(m_Position, vp.m_Position);
+	std::swap(static_cast<Framebuffer&>(*this), static_cast<Framebuffer&>(vp));
 }
 
 Viewport& Viewport::operator=(Viewport&& vp) noexcept {
@@ -27,13 +27,13 @@ void Viewport::display(const std::string& windowName, uint32_t attachmentID) {
 
 	// Check if it needs to resize
 	ImVec2 port = ImGui::GetContentRegionAvail();
-	ImGui::Image((void*)(uintptr_t)m_Buffer.texture(attachmentID).id(), port, { 0.0f, 1.0f }, { 1.0f, 0.0f });
+	ImGui::Image((void*)(uintptr_t) texture(attachmentID).id(), port, { 0.0f, 1.0f }, { 1.0f, 0.0f });
 
-	glm::uvec2 view = m_Buffer.size();
+	glm::uvec2 view = size();
 	glm::uvec2 uport{ uint32_t(port.x), uint32_t(port.y) };
 
 	if (uport.x != view.x || uport.y != view.y) {
-		m_Buffer.resize(uport);
+		resize(uport);
 	}
 
 	// In case the windows moved
